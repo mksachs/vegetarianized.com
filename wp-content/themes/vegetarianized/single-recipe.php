1c944b24e7ca808bdaec5583a6a81bd555a14fd1
Michael Sachs <msachs@discoverydn.com>
2014-08-13 08:49:06 -0700

    added shareaholic

--- wp-content/themes/vegetarianized/single-recipe.php
+++ wp-content/themes/vegetarianized/single-recipe.php
@@ -133,9 +133,12 @@
 <!-- Links to share the recipe -->
     <div id="share">
         <div class="column1">
-            <!-- Facebook -->
-            <div class="fb-like" data-href="<?php echo get_permalink($post->ID); ?>" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="false" data-send="false"></div>
 
+            <?php echo do_shortcode("[shareaholic app=\"share_buttons\" id=\"7452915\"]"); ?>
+
+            <!-- Facebook 
+            <div class="fb-like" data-href="<?php echo get_permalink($post->ID); ?>" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="false" data-send="false"></div>
+            -->
             <!-- Place this tag where you want the +1 button to render. 
             <div class="g-plusone" data-size="medium" data-href="<?php echo get_permalink($post->ID); ?>"></div>-->
 
@@ -148,9 +151,10 @@
               })();
             </script>-->
 
-            <!-- Twitter -->
+            <!-- Twitter 
             <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo get_permalink($post->ID); ?>">Tweet</a>
             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+            -->
             
         </div>
         <div class="column2">
