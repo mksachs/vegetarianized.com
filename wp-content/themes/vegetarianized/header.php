9fbd05a4b14b03117dfb8ee50d5e610941504d17
Michael Sachs <msachs@discoverydn.com>
2014-08-13 09:11:15 -0700

    follow buttons

--- wp-content/themes/vegetarianized/header.php
+++ wp-content/themes/vegetarianized/header.php
@@ -116,7 +116,9 @@ $logo_print_image =  get_template_directory_uri()."/images/vegetarianized_logo_"
                             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
                         </li>
                         -->
-                        <?php echo do_shortcode("[shareaholic app='follow_buttons' id='7515554']"); ?>
+                        <li>
+                        <?php echo do_shortcode("[shareaholic app='follow_buttons' id='7452915']"); ?>
+                        </li>
 
                     </ul>
                 </div>
