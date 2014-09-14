5c9da05e575027cba76ddc52558b753849ac40a0
Michael Sachs <msachs@discoverydn.com>
2014-09-14 14:55:26 -0700

    subscribe

--- wp-content/themes/vegetarianized/header.php
+++ wp-content/themes/vegetarianized/header.php
@@ -63,6 +63,17 @@ $logo_print_image =  get_template_directory_uri()."/images/vegetarianized_logo_"
                     <?php get_search_form(); ?>
                 </div>
             </div>
+            <div class="nav_item">
+                Subscribe
+                <div class="nav_sub_item">
+                    <ul>
+                        <li>
+                        <?php echo do_shortcode("[wysija_form id=\"3\"]"); ?>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
             <!--
             <div class="nav_item">
                 Social Media
